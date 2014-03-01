5c96628ea10e6150ecef8663447882c0fd421295
musikar <apple@appledeMacBook-Pro.local>
2014-03-01 14:51:21 +0800

    hi

---
+++ xcode/xcode/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  xcode
+//
+//  Created by musikar on 14-3-1.
+//  Copyright (c) 2014年 musikar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
